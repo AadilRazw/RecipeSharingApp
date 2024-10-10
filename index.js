85c3e578915bf6ef0d593c9141d7908579f8d4d2
aadilrazw <idrisiaadil2@gmail.com>
2024-10-10 19:35:11 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const recipes = require("./data/MOCK_DATA.json")
+const cors = require('cors')
+
+const app = express()
+app.use(cors({
+    origin:"https://4efe-122-161-73-233.ngrok-free.app"
+}))
+
+const port = 8080;
+
+
+
+app.get('/api/v1/allRecipes',(req,res)=>{
+    res.json(recipes)
+})
+
+
+app.get('/api/v1/breakfast',(req,res)=>{
+    const filteredData = recipes.filter((recipe)=>recipe.mealType.includes('Breakfast'))
+    res.json(filteredData)
+})
+
+
+app.get('/api/v1/lunch',(req,res)=>{
+    const filteredData = recipes.filter((recipe)=>recipe.mealType.includes('Lunch'))
+    res.json(filteredData)
+})
+
+
+app.get('/api/v1/dinner',(req,res)=>{
+    const filteredData = recipes.filter((recipe)=>recipe.mealType.includes('Dinner'))
+    res.json(filteredData)
+})
+
+app.get('/api/v1/recipe/:id',(req,res)=>{
+    const id = req.params.id;
+    console.log(id)
+    const recipe = recipes.find(recipe=>recipe.id == id);
+    console.log(recipe)
+    res.json(recipe)
+})
+
+
+app.listen(port, ()=>{
+    console.log("app listening at port "+port )
+})
